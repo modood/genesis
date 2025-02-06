c1a62c30b2eccd9a2d06c8256de566503ca99180
modood <modood@qq.com>
2025-02-06 11:47:07 +0800

    Update docker installation script and use Tsinghua mirror

--- ubuntu_step16_docker.sh
+++ ubuntu_step16_docker.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Add user account to the docker group
-sudo usermod -aG docker $(whoami)
+(command -v docker >/dev/null 2>&1) || {
+  # Add Docker's official GPG key:
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# set mirror
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s https://hub-mirror.c.163.com
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# https://github.com/docker/compose
-(command -v docker-compose >/dev/null 2>&1) || {
-  echo -e "\nInstalling docker-compose..."
-  sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  # Add user account to the docker group
+  sudo usermod -aG docker $(whoami)
+
+  # set mirror
+  # curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s https://hub-mirror.c.163.com
 }
 
