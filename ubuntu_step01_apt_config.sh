08fe43175a19249cb968ddb182deca12f3f5acb0
modood <modood@qq.com>
2025-02-06 11:45:05 +0800

    Update apt configuration and software installation

--- ubuntu_step01_apt_config.sh
+++ ubuntu_step01_apt_config.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-echo -e "Backuping existing apt configuration"
-timestr=$(date +%Y%m%d%H%M)
-sudo tar -zcPf /etc/apt.$timestr.tar.gz /etc/apt
-
-aptKeys=(
-  https://download.docker.com/linux/ubuntu/gpg # docker-ce
-)
-for k in ${aptKeys[@]}; do echo "Adding apt key: ${k}"; curl -fsSL $k | sudo apt-key add -; done
-
 aptRepositories=(
-  ppa:jonathonf/vim # vim
+  ppa:graphics-drivers/ppa # nvidia-graphics-drivers
   ppa:git-core/ppa # git
   ppa:ansible/ansible # ansible
-  ppa:ubuntuhandbook1/mpv # mpv
+  ppa:flatpak/stable # flatpak
   ppa:obsproject/obs-studio # obs-studio
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # docker-ce
+  ppa:ubuntuhandbook1/mpv # mpv
+  ppa:ubuntuhandbook1/vlc # vlc
 )
 for ((i = 0; i < ${#aptRepositories[@]}; i++));
 do
   echo "Adding apt repository: ${aptRepositories[$i]}"
   sudo add-apt-repository -y "${aptRepositories[$i]}"
 done
-
 
--- ubuntu_step02_apt_install.sh
+++ ubuntu_step02_apt_install.sh
@@ -6,7 +6,6 @@ aptPackages=(
 ############################################################################################################################################
   sudo                        # sudo                                  Provide limited super user privileges to specific users
   lsb-release                 # lsb_release                           Linux Standard Base version reporting utility
-  lsb-core                    #                                       Linux Standard Base core support package
   net-tools                   # ifconfig, netstat, ...                NET-3 networking toolkit
   telnet                      # telnet                                basic telnet client
   wget                        # wget                                  retrieves files from the web
@@ -34,6 +33,7 @@ aptPackages=(
   ca-certificates             # update-ca-certificates                Common CA certificates
   software-properties-common  # add-apt-repository                    manage the repositories that you install software from (common)
   gdebi                       # gdebi                                 simple tool to view and install deb files - GNOME GUI
+  flatpak                     # flatpak                               Application deployment framework for desktop apps
 
   sysstat                     # sar, mpstat, iostat, pidstat, ...     system performance tools for Linux
   nmon                        # nmon                                  performance monitoring tool for Linux
@@ -41,6 +41,8 @@ aptPackages=(
   atop                        # atop, atopsar                         Monitor for system resources and process activity
   iotop                       # iotop                                 simple top-like I/O monitor
   iftop                       # iftop                                 displays bandwidth usage information on an network interface
+  nvtop                       # nvtop                                 interactive GPU process viewer
+  lm-sensors                  # sensors, sensors-detect               utilities to read temperature/voltage/fan sensors
   nethogs                     # nethogs                               Net top tool grouping bandwidth per process
   ethtool                     # ethtool                               display or change Ethernet device settings
   nicstat                     # nicstat                               print network traffic statistics
@@ -51,11 +53,9 @@ aptPackages=(
   colordiff                   # colordiff                             tool to colorize 'diff' output
   bwm-ng                      # bwm-ng                                small and simple console-based bandwidth monitor
   corkscrew                   # corkscrew                             tunnel TCP connections through HTTP proxies
-# proxychains                 # proxychains3                          proxy chains - redirect connections through proxy servers
   proxychains-ng              # proxychains4                          proxychains ng (new generation) - a preloader which hooks calls to sockets in dynamically linked programs and redirects it through one or more socks/http proxies
   tor                         # tor                                   anonymizing overlay network for TCP
 
-  python-pip                  # pip, pip2                             alternative Python package installer
   python3-pip                 # pip3                                  alternative Python package installer - Python 3 version of the package
   pipenv                      # pipenv, pipenv-resolver, pewtwo       Python's officially recommended packaging tool
   tmux                        # tmux                                  terminal multiplexer
@@ -64,7 +64,6 @@ aptPackages=(
   autojump                    # autojump                              shell extension to jump to frequently used directories
   ack-grep                    # ack, ack-grep                         grep-like program specifically for large source trees
   vim                         # vim                                   Vi IMproved - enhanced vi editor
-  vim-gtk                     # vim-gtk                               Vi IMproved - enhanced vi editor - with GTK2 GUI
   exuberant-ctags             # ctags, ctags-exuberant                build tag file indexes of source code definitions
   ansible                     # ansible, ansible-playbook, ...        Configuration management, deployment, and task execution system
   fcitx                       # fcitx, fcitx-configtool, ...          Flexible Input Method Framework
@@ -91,10 +90,6 @@ aptPackages=(
   v4l2loopback-dkms           #                                       Source for the v4l2loopback driver (DKMS)
   ffmpeg                      # ffmpeg, ffplay, ffprobe, ...          Tools for transcoding, streaming and playing of multimedia files
   obs-studio                  # obs                                   recorder and streamer for live video content
-
-  docker-ce
-  docker-ce-cli
-  containerd.io
 )
 
 sudo apt update
