df09f8cea6e35146e21e672e179f39c372509d3e
modood <modood@qq.com>
2025-05-22 17:56:53 +0800

    Upgrade tmux to 3.5a, nvm to 0.40.1, node.js to 22, and go to 1.24.3

--- ubuntu_step04_tmux_upgrade.sh
+++ ubuntu_step04_tmux_upgrade.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ `tmux -V` != "tmux 3.2" ]]; then
-  curl -L https://github.com/tmux/tmux/releases/download/3.2/tmux-3.2.tar.gz -o tmux-3.2.tar.gz
-  tar -zxvf tmux-3.2.tar.gz
-  cd tmux-3.2
+if [[ `tmux -V` != "tmux 3.5a" ]]; then
+  curl -L https://github.com/tmux/tmux/releases/download/3.5a/tmux-3.5a.tar.gz -o tmux-3.5a.tar.gz
+  tar -zxvf tmux-3.5a.tar.gz
+  cd tmux-3.5a
   sudo apt install -y libevent-dev ncurses-dev
   sudo ./configure
   sudo make
   sudo make install
-  cd .. && sudo rm -rf tmux-3.2 tmux-3.2.tar.gz
+  cd .. && sudo rm -rf tmux-3.5a tmux-3.5a.tar.gz
 fi
 
--- ubuntu_step08_nvm.sh
+++ ubuntu_step08_nvm.sh
@@ -3,13 +3,14 @@
 # https://github.com/creationix/nvm
 if [ ! -d $HOME/.nvm ]; then
   echo -e "\nInstalling nvm..."
-  curl -L https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+  curl -L https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-  nvm install 16
-  nvm use 16
-  nvm alias default 16
+  nvm install 22
+  nvm use 22
+  nvm alias default 22
 fi
 
